cb31090b1ae491b48eb4d1cae6fb40a759289b8c
Student <none>
2024-01-20 11:38:32 -0500

    added settings to preferences and read the current position

--- src/main/java/frc/robot/subsystems/FlySwatter.java
+++ src/main/java/frc/robot/subsystems/FlySwatter.java
@@ -6,10 +6,12 @@ package frc.robot.subsystems;
 
 import java.util.Map;
 
+import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.controls.MotionMagicVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 
+import edu.wpi.first.wpilibj.Preferences;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
@@ -23,6 +25,9 @@ public class FlySwatter extends SubsystemBase {
   private Position targetPosition = Position.LOW;
   private TalonFX primary = new TalonFX(Constants.CanBus.FLYSWATTER_PRIMARY);
   private TalonFX secondary = new TalonFX(Constants.CanBus.FLYSWATTER_SECONDARY);
+  private static final double POSITION_ERROR_DELTA = 0.1;
+
+  private StatusSignal<Double> position = primary.getPosition();
 
   private Map<Position,Double> positionValues = Map.of(
     Position.LOW, 0.0,
@@ -32,6 +37,20 @@ public class FlySwatter extends SubsystemBase {
 
   /** Creates a new FlySwatter. */
   public FlySwatter() {
+    if ( !Preferences.containsKey("FLySwatter/Position/Low") ){
+      Preferences.setDouble("FLySwatter/Position/Low", 0);
+    }
+    if ( !Preferences.containsKey("FLySwatter/Position/Medium") ){
+      Preferences.setDouble("FLySwatter/Position/Medium", 0);
+    }
+    if ( !Preferences.containsKey("FLySwatter/Position/High"));{
+      Preferences.setDouble("FLySwatter/Position/High", 0);
+    }
+    
+    positionValues.put(Position.LOW, Preferences.getDouble("FLySwatter/Position/Low", 0));
+    positionValues.put(Position.MEDIUM, Preferences.getDouble("FLySwatter/Position/Medium", 0));
+    positionValues.put(Position.HIGH, Preferences.getDouble("FLySwatter/Position/High", 0));
+
     secondary.setControl(new Follower(Constants.CanBus.FLYSWATTER_PRIMARY, false));
   }
 
@@ -43,12 +62,14 @@ public class FlySwatter extends SubsystemBase {
 
   public boolean isAtPosition()
   {
-   
-   return false;
+        double current = position.refresh().getValue();
+        double target = positionValues.get(targetPosition);
+
+        return Math.abs(target - current) < POSITION_ERROR_DELTA;
   }
 
-  public void stop()
-  {
+
+  public void stop() {
     primary.set(0);
   }
 
