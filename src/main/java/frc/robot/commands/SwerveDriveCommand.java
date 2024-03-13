feef9952702f19e74bfcfc461e17ab1fc43da9db
Student <none>
2024-03-12 20:38:04 -0400

    auto changes

--- src/main/java/frc/robot/commands/SwerveDriveCommand.java
+++ src/main/java/frc/robot/commands/SwerveDriveCommand.java
@@ -32,6 +32,7 @@ public class SwerveDriveCommand extends Command {
 
   // Called when the command is initially scheduled.
   @Override
+  
   public void initialize() {
     SmartDashboard.putBoolean("Drive by Field", fieldOriented);
   }
