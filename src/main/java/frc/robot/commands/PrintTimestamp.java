b3d20224ec32421c91e960a5e170184599e704a1
Finney Student <none>
2024-05-21 20:54:36 -0400

    I have access to Steves GitHub now

--- src/main/java/frc/robot/commands/PrintTimestamp.java
+++ src/main/java/frc/robot/commands/PrintTimestamp.java
@@ -28,6 +28,7 @@ public class PrintTimestamp extends Command {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {}
+  
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
