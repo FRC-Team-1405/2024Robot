36cd96a7f2da93f3e9669d72058aedbd4274fef9
Stephen Cerbone <importsjc@gmail.com>
2025-01-24 18:16:20 -0500

    updating to 2025

--- src/main/java/com/team254/frc2024/BuildConstants.java
+++ src/main/java/com/team254/frc2024/BuildConstants.java
@@ -5,15 +5,15 @@ package com.team254.frc2024;
  */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "FRC-2024";
+  public static final String MAVEN_NAME = "cheesypoofs";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 700;
-  public static final String GIT_SHA = "821115272f12f91663fe6ee1328a78a2f00ce98a";
-  public static final String GIT_DATE = "2024-09-09 20:33:09 PDT";
+  public static final int GIT_REVISION = 2;
+  public static final String GIT_SHA = "040f653744c9b18182be5f6bc51a7e505e346e59";
+  public static final String GIT_DATE = "2024-10-03 22:00:05 PDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2024-10-03 21:54:43 PDT";
-  public static final long BUILD_UNIX_TIME = 1728017683796L;
-  public static final int DIRTY = 0;
+  public static final String BUILD_DATE = "2025-01-24 15:00:29 PST";
+  public static final long BUILD_UNIX_TIME = 1737759629087L;
+  public static final int DIRTY = 128;
 
   private BuildConstants(){}
 }
